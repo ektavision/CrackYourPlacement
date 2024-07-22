9547da4bafb1b69fd32ef81e53b6cbcade2c8e63
Ekta <60689008+ektavision@users.noreply.github.com>
2024-07-22 22:39:08 +0530

    Add files via upload

---
+++ Arrays/ReversePairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reversePairs(int[] nums) {
+         if (nums == null || nums.length == 0) return 0;
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+     private int mergeSort(int[] nums, int start, int end) {
+        if (start >= end) return 0;
+        int mid = start + (end - start) / 2;
+        int count = mergeSort(nums, start, mid) + mergeSort(nums, mid + 1, end);
+        for (int i = start, j = mid + 1; i <= mid; i++) {
+            while (j <= end && nums[i] / 2.0 > nums[j]) j++; 
+            count += j - (mid + 1); 
+        }
+        
+        Arrays.sort(nums, start, end + 1); 
+        
+        return count;
+    }
+}
