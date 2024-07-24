d2f0fa35e0a7507690ee0e041f7e81ca4eeecf46
Ekta <60689008+ektavision@users.noreply.github.com>
2024-07-24 23:00:12 +0530

    Add files via upload

---
+++ String/SimplifyPath.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        String[] components = path.split("/");
+
+        for (String component : components) {
+            if (component.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else if (!component.isEmpty() && !component.equals(".")) {
+                stack.push(component);
+            }
+        }
+
+        StringBuilder result = new StringBuilder("/");
+        for (String dir : stack) {
+            result.append(dir).append("/");
+        }
+
+        if (result.length() > 1) {
+            result.setLength(result.length() - 1);
+        }
+
+        return result.toString();  
+    }
+}
