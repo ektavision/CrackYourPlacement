5cdd63f33502da1fd9914d1efa2acda7129b6446
Ekta <60689008+ektavision@users.noreply.github.com>
2024-07-24 22:30:15 +0530

    Add files via upload

---
+++ String/GenerateParentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    private void generateParenthesis(List<String> result, String s, int open, int close, int n) {
+        if (open == n && close == n) {
+            result.add(s);
+            return;
+        }
+        if (open < n) {
+            generateParenthesis(result, s + "(", open + 1, close, n);
+        }
+        if (close < open) {
+            generateParenthesis(result, s + ")", open, close + 1, n);
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+          List<String> result = new ArrayList<>();
+        generateParenthesis(result, "", 0, 0, n);
+        return result;
+    }
+}
