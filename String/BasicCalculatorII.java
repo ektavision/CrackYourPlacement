2ce191e58d4c763d52b39c6bbdd327551ec5cf6e
Ekta <60689008+ektavision@users.noreply.github.com>
2024-07-26 11:45:28 +0530

    Add files via upload

---
+++ String/BasicCalculatorII.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int calculate(String s) {
+          char[] arr = s.toCharArray();
+        Stack<Integer> stack = new Stack<>();
+        char operator = '+';
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ' ')
+                continue;
+            if (Character.isDigit(arr[i])) {
+                StringBuilder buildNum = new StringBuilder();
+                while (i < arr.length && Character.isDigit(arr[i])) {
+                    buildNum.append(arr[i++]);
+                }
+                i--;
+
+                int curNum = Integer.parseInt(buildNum.toString());
+                if (operator == '-') {
+                    curNum *= -1;
+                } else if (operator == '*') {
+                    curNum *= stack.pop();
+                } else if (operator == '/') {
+                    curNum = stack.pop() / curNum;
+                }
+                stack.push(curNum);
+            } else {
+                operator = arr[i];
+            }
+        }
+
+        int total = 0;
+        while (!stack.isEmpty()) {
+            total += stack.pop();
+        }
+        return total;
+    }
+}
