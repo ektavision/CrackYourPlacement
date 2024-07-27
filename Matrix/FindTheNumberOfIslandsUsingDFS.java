fa2c919a75a95b5a205d800a5042fd1ef55e662b
Ekta <60689008+ektavision@users.noreply.github.com>
2024-07-27 22:31:15 +0530

    Add files via upload

---
+++ Matrix/FindTheNumberOfIslandsUsingDFS.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class Main {
+    
+    static void DFS(int[][] M, int i, int j, int ROW,
+                    int COL)
+    {
+        if (i < 0 || j < 0 || i > (ROW - 1) || j > (COL - 1)
+            || M[i][j] != 1) {
+            return;
+        }
+
+        if (M[i][j] == 1) {
+            M[i][j] = 0;
+            DFS(M, i + 1, j, ROW,
+                COL); 
+            DFS(M, i - 1, j, ROW,
+                COL); 
+            DFS(M, i, j + 1, ROW,
+                COL); 
+            DFS(M, i, j - 1, ROW,
+                COL); 
+            DFS(M, i + 1, j + 1, ROW,
+                COL); 
+            DFS(M, i - 1, j - 1, ROW,
+                COL);
+            DFS(M, i + 1, j - 1, ROW,
+                COL); 
+            DFS(M, i - 1, j + 1, ROW,
+                COL); 
+        }
+    }
+
+    static int countIslands(int[][] M)
+    {
+        int ROW = M.length;
+        int COL = M[0].length;
+        int count = 0;
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (M[i][j] == 1) {
+                    count++;
+                    DFS(M, i, j, ROW,
+                        COL); 
+                }
+            }
+        }
+        return count;
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        int[][] M =  {{1, 1, 0, 0, 0},
+                           {0, 1, 0, 0, 1},
+                           {1, 0, 0, 1, 1},
+                          {0, 0, 0, 0, 0},
+                         {1, 0, 1, 0, 0}};
+
+        System.out.print( countIslands(M));
+    }
+}
