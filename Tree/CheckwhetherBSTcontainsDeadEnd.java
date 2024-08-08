41dc0c9a703cc1718198534f13430bcb9d0d07c7
Ekta <60689008+ektavision@users.noreply.github.com>
2024-08-08 22:05:21 +0530

    Add files via upload

---
+++ Tree/CheckwhetherBSTcontainsDeadEnd.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public static boolean isDeadEnd(Node root)
+    {
+        //Add your code here.
+         return solve(root, 1, Integer.MAX_VALUE);
+    }
+     public static boolean solve(Node root, int min, int max){
+        if(root == null) 
+            return false;
+        if(min == max) 
+            return true;
+        boolean left = solve(root.left, min, root.data - 1);
+        boolean right = solve(root.right, root.data + 1, max);
+        return left || right;
+    }
+}
