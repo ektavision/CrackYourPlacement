d754404ce26d411b9007643435257ee5b0037f1f
Ekta <60689008+ektavision@users.noreply.github.com>
2024-08-08 20:02:17 +0530

    Add files via upload

---
+++ Tree/BalancedBinaryTree.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+         if (root==null) {
+            return true;
+        }
+        if (Math.abs(f(root.left)-f(root.right))>1) {
+            return false;
+        }
+        return isBalanced(root.left)&&isBalanced(root.right);
+    }
+        public int f(TreeNode root){
+        if (root==null) {
+            return 0;
+        }else {
+            return 1+Math.max(f(root.left), f(root.right));
+        }
+    }
+}
