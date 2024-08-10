f4013d9d71d247a7ec01479c5add9424d6f3ab0e
Ekta <60689008+ektavision@users.noreply.github.com>
2024-08-10 19:57:30 +0530

    Add files via upload

---
+++ Tree/UniqueBinarySearchTrees.java
@@ -0,0 +1,23 @@
+class Solution {
+    Integer[] cache;
+  public int numTrees(int n) {
+     cache = new Integer[n + 1];
+      return dfs(n);  
+  }
+   private int dfs(int n){
+      if(n < 2) return 1;
+      if(cache[n] != null) return cache[n];
+      
+      int total = 0;
+      for(int curNum = 1; curNum <= n; curNum++){
+          int leftN = curNum - 1;
+          int leftTotalBST = dfs(leftN);
+          int rightN = n - curNum;
+          int rightTotalBST = dfs(rightN);
+          total += (leftTotalBST * rightTotalBST);
+      }
+      
+      cache[n] = total;
+      return total;
+  }
+}
