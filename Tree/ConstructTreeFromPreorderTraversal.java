45d9a8ec3ad5c6470b59b92df6f940993cc466d9
Ekta <60689008+ektavision@users.noreply.github.com>
2024-08-13 20:05:01 +0530

    Add files via upload

---
+++ Tree/ConstructTreeFromPreorderTraversal.java
@@ -0,0 +1,22 @@
+
+class Solution{
+    int index = 0;
+
+
+Node constructTree(int n, int pre[], char preLN[]){
+    // code here
+       if (index >= n) {
+        return null;
+    }
+
+    Node newNode = new Node(pre[index]);
+    index++;
+
+    if (preLN[index - 1] == 'N') {
+        newNode.left = constructTree(n, pre, preLN);
+        newNode.right = constructTree(n, pre, preLN);
+    }
+
+    return newNode;
+}
+}
