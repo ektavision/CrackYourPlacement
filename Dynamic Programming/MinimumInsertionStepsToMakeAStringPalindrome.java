f05e0718f732baa1103407274953a25419ff1131
Ekta <60689008+ektavision@users.noreply.github.com>
2024-08-27 13:10:24 +0530

    Add files via upload

---
+++ Dynamic Programming/MinimumInsertionStepsToMakeAStringPalindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minInsertions(String s) {
+         int n = s.length();
+        int[][] dp = new int[n][n];
+        
+        for (int L = 2; L <= n; L++) {
+            for (int i = 0; i < n - L + 1; i++) {
+                int j = i + L - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[0][n - 1];
+    }
+}
